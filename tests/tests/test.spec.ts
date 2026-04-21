4e1a314fb0e1539a0bc429be87baa37fe563e77f
azeljkovic <aleksandar.zeljkovic91@gmail.com>
2026-04-21 10:40:06 +0200

    add login tests for valid credentials

--- tests/tests/test.spec.ts
+++ tests/tests/test.spec.ts
@@ -10,7 +10,13 @@ const validCredentials = {
   password: 'admin123',
 };
 
+const routes = {
+  login: '**/api/login*',
+}
+
+
 test('invalid login', async ({page}) => {
+  const requestPromise = page.waitForResponse(routes.login);
   await page.goto('/');
 
   await page.getByRole('textbox', {name: '* Email'}).fill(invalidCredentials.email);
@@ -18,9 +24,35 @@ test('invalid login', async ({page}) => {
 
   await page.getByRole('button', {name: 'Log In'}).click();
 
+  const req = await requestPromise;
+  expect(req.status()).toBe(404);
+  const body = await req.json();
+  expect(body.message).toContain('No account with this email has been registered.');
+
   const alert = page.getByRole('alert');
 
   await expect(alert).toBeVisible();
   await expect(alert).toContainText('No account with this email has been registered.');
 
 });
+
+test('valid login', async ({page}) => {
+  const requestPromise = page.waitForResponse(routes.login);
+
+  await page.goto('/');
+
+  await page.getByRole('textbox', {name: '* Email'}).fill(validCredentials.email);
+  await page.getByRole('textbox', {name: '* Password'}).fill(validCredentials.password);
+
+  await page.getByRole('button', {name: 'Log In'}).click();
+
+  const req = await requestPromise;
+  expect(req.status()).toBe(200);
+  const body = await req.json();
+  expect(body.message).toContain('Successfully login user');
+
+  const alert = page.getByRole('alert');
+
+  await expect(alert).toBeHidden();
+  await expect(page.getByRole('button', {name: 'Log In'})).not.toBeVisible();
+})
